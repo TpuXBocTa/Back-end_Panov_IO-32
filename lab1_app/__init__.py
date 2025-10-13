d2d6cfade2f8e8fec2f4d472e7ad6e911d7e5445
Панов Микита <panov.mykyta@gmail.com>
2025-10-13 17:16:02 +0300

    Minimal app

---
+++ lab1_app/__init__.py
@@ -0,0 +1,4 @@
+from flask import Flask
+
+app = Flask(__name__)
+import lab1_app.views
---
+++ lab1_app/views.py
@@ -0,0 +1,5 @@
+from lab1_app import app
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
