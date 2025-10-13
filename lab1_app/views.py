2eb58af3dcfeed17eb3e51144ecaa99aacb0b476
Панов Микита <panov.mykyta@gmail.com>
2025-10-13 20:59:18 +0300

    Healthcheck

--- lab1_app/views.py
+++ lab1_app/views.py
@@ -1,5 +1,13 @@
 from lab1_app import app
+from datetime import datetime
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/healthcheck")
+def health_check():
+    return {
+        "status": "OK",
+        "timestamp": datetime.now(),
+    }
